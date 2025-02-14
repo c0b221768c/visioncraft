0acd14982eb766a56791e6731c86a05883f58bfc
c0b221768c <c0b221768c@edu.teu.ac.jp>
2025-02-14 19:33:23 +0900

    Fix: *.sh did not work so I fixed the path.

--- commands/docker-build.sh
+++ commands/docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Remove the container && Rebuild && Reboot"
-docker compose -f compose.yml down
+docker compose -f docker-compose.yml down
 
 echo "Build the container"
-docker compose -f compose.yml up --build -d
+docker compose -f docker-compose.yml up --build -d
 
 echo "Done!"
 docker ps
--- commands/docker-exit.sh
+++ commands/docker-exit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Stop container && Remove"
-docker compose -f docker/docker-compose.yml down
+docker compose -f docker-compose.yml down
 
 echo "Done!"
